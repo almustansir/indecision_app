baaf80ea80f311dfab99d13fa7309df94680f314
Samit_08 <almustansir465@gmail.com>
2022-08-22 11:05:32 -0700

    restored to props

--- indecision/src/App.js
+++ indecision/src/App.js
@@ -1,57 +1,56 @@
-import React, { Component } from 'react'
-import 'normalize.css/normalize.css'
+import React, { Component } from 'react';
+import 'normalize.css/normalize.css';
 import HeaderComp from './components/HeaderComp';
 import ActionComp from './components/ActionComp';
 import OptionsComp from './components/OptionsComp';
 import AddOptionComp from './components/AddOptionComp';
-// import OptionModalComp from './components/OptionModalComp';
-import "./styles/style.scss"
+import OptionModalComp from './components/OptionModalComp';
+import "./styles/style.scss";
 
 export default class App extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.actionFunction = this.actionFunction.bind(this)
-  //   this.modalCloseFunction = this.modalCloseFunction.bind(this)
-  // }
+  constructor(props) {
+    super(props)
+    this.actionFunction = this.actionFunction.bind(this)
+    this.modalCloseFunction = this.modalCloseFunction.bind(this)
+  }
 
-  // state = {
-  //   // make database
-  //   // should fetch data from database
-  //   "options": ["Learn React", "Learn Redux", "Learn FireBase"],
-  //   "selectedOption": undefined
-  // }
+  state = {
+    // make database
+    // should fetch data from database
+    "selectedOption": undefined
+  }
 
-  // modalCloseFunction() {
-  //     this.setState({ selectedOption: undefined })
-  // }
+  modalCloseFunction() {
+      this.setState({ selectedOption: undefined })
+  }
 
 
   // Add Function
-  // actionFunction(num) {
-  //     // turn this into a mudol later
-  //     const op = this.state.options[num]
-  //     this.setState(() => ({
-  //         selectedOption: op
-  //     }))
-  // }
+  actionFunction(num) {
+      // turn this into a mudol later
+      const op = this.props.options[num]
+      this.setState(() => ({
+          selectedOption: op
+      }))
+  }
 
 
   render() {
+    let options = this.props.options
     return (
       <div>
         <HeaderComp />
             <div className='container'>
-                {/* <ActionComp OptionsLength={this.state.options.length} actionFunction={this.actionFunction}/> */}
-                <ActionComp />
+                <ActionComp OptionsLength={options.length} actionFunction={this.actionFunction}/>
                 <div className='wedget'>
                     <OptionsComp />
                     <AddOptionComp />
                 </div>
             </div>
-            {/* <OptionModalComp
+            <OptionModalComp
                 selectedOption={this.state.selectedOption}
                 modalCloseFunction={this.actionFunction}
-            /> */}
+            />
       </div>
     )
   }
--- indecision/src/index.js
+++ indecision/src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './app/store'
+import store from './app/store';
+import { useSelector } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const options = useSelector((state) => state.options.value)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App options={options}/>
     </Provider>
   </React.StrictMode>
 );
